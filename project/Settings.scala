a94648e292abdfe4881d8e11fa9f06846102cb6c
Harshad Deo <subterranean.hominid@gmail.com>
2019-05-06 15:31:11 +0530

    bump version to 0.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,6 +1,6 @@
 object Settings {
   object versions {
-    val project: String = "0.1.0-SNAPSHOT"
+    val project: String = "0.1.0"
     val mathbridge: String = "0.2.3"
     val scala: String = "2.12.8"
     val scalatest: String = "3.0.5"
